ddc529281302fb8f54bfde96e5c7361d53e10df7
harishbabu2007 <harishbabubalaji@gmail.com>
2026-06-03 17:45:33 +0000

    fix in max embedding position

--- architecture/embedding.py
+++ architecture/embedding.py
@@ -3,7 +3,7 @@ import torch
 import math
 
 class SinusoidalPositionalEncoding(nn.Module):
-    def __init__(self, d_model, max_len=5000):
+    def __init__(self, d_model, max_len=20000):
         super().__init__()
         pe = torch.zeros(max_len, d_model)
         position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
