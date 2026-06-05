18b9f5e9d5f8fd02d4d70064e22595e32cf54c65
harishbabu2007 <harishbabubalaji@gmail.com>
2026-06-05 11:12:37 +0000

    Implemented new trainer for new version of model with optional pwm input

--- losses.py
+++ losses.py
@@ -23,3 +23,12 @@ def masked_ppm_loss(logits, target_pwm, mask):
     loss = -(target_ppm * log_probs).sum(dim=-1)
 
     return loss.mean()
+
+def masked_ppm_loss_with_one_hot(logits, target_pwm, mask, pwm_present):
+    if pwm_present:
+        return masked_ppm_loss(logits, target_pwm, mask)
+    
+    log_probs = F.log_softmax(logits.squeeze(0), dim=-1)
+
+    loss = -(target_pwm * log_probs).sum(dim=-1)
+    return loss.mean()
